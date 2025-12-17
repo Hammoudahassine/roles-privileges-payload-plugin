33facf6fa1ed6e1c0be64e15ed08ef9f5d3f5e23
hammoudahassine7 <hammoudahassine7@gmail.com>
2025-12-17 12:10:39 +0100

    feat: initial commit

---
+++ dev/app/(payload)/admin/importMap.js
@@ -0,0 +1,9 @@
+import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'roles-privileges-payload-plugin/client'
+import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'roles-privileges-payload-plugin/rsc'
+
+export const importMap = {
+  'roles-privileges-payload-plugin/client#BeforeDashboardClient':
+    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
+  'roles-privileges-payload-plugin/rsc#BeforeDashboardServer':
+    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+}
